e517a475ee4d8486f6e7b79a1aaaaf74e3df333f
bmansvelt <64210209+bmansvelt@users.noreply.github.com>
2020-11-13 19:12:33 +0200

    Update mpi_particles.sh

--- testing/mpi_particles.sh
+++ testing/mpi_particles.sh
@@ -9,18 +9,18 @@
 
 module load mpi/openmpi-4.0.1
 
-echo "cores, pop_size, num_particles, migration_interval, migration_rate, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_particles.txt
+echo "cores, pop_size, num_particles, migration_interval, migration_rate, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_particles.txt
 
 make particle_mpi
 
 # different number of particles
-for i in 50 100 200 500 700 1000
+for i in 8 16 32 64 128
 do
-	mpirun /home/hpc36/CSC_HPC_Project/mpi/particle_mpi 5000 100 100 $i 1 50 0.05 >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_particles.txt
-	echo "mpi: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_log.txt
+	mpirun /home/hpc36/CSC_HPC_Project/mpi/particle_mpi 2000 100 100 $i 1 50 0.05 >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_particles.txt
+	echo "mpi: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_log.txt
 done 
 
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_particles.txt
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_log.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_particles.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_log.txt
 
 rm /home/hpc36/CSC_HPC_Project/mpi/particle_mpi
