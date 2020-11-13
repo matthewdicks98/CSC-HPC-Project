330da2465d0b7415f0f352ef5b27a55a6e21f35f
bmansvelt <64210209+bmansvelt@users.noreply.github.com>
2020-11-13 19:16:16 +0200

    Update omp_crossover_particles.sh

--- testing/omp_crossover_particles.sh
+++ testing/omp_crossover_particles.sh
@@ -7,19 +7,19 @@
 #SBATCH --mail-user=mnsbon012@myuct.ac.za
 #SBATCH --mail-type=ALL
 
-echo "cores, pop_size, num_particles, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results/omp/omp_crossover_particles.txt
+echo "cores, pop_size, num_particles, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results2.0/omp/omp_crossover_particles.txt
 
 make particle_omp_crossover
 
 # different particle size
-for i in 50 100 200 500 700 1000
+for i in 8 16 32 64 128
 do 
 	export OMP_NUM_THREADS=32
-	/home/hpc36/CSC_HPC_Project/openmp/particle_omp_crossover 5000 100 100 $i 1 >> /home/hpc36/CSC_HPC_Project/Results/omp/omp_crossover_particles.txt
-	echo "omp_crossover: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results/omp/omp_log.txt
+	/home/hpc36/CSC_HPC_Project/openmp/particle_omp_crossover 2000 100 100 $i 1 >> /home/hpc36/CSC_HPC_Project/Results2.0/omp/omp_crossover_particles.txt
+	echo "omp_crossover: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results2.0/omp/omp_log.txt
 done 
 
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/omp/omp_crossover_particles.txt
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/omp/omp_log.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/omp/omp_crossover_particles.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/omp/omp_log.txt
 
 rm /home/hpc36/CSC_HPC_Project/openmp/particle_omp_crossover
