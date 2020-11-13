848a602c33a59acbae293e6c2dcbc2d99518029c
bmansvelt <64210209+bmansvelt@users.noreply.github.com>
2020-11-13 19:17:03 +0200

    Update ser_particles.sh

--- testing/ser_particles.sh
+++ testing/ser_particles.sh
@@ -7,18 +7,18 @@
 #SBATCH --mail-user=mnsbon012@myuct.ac.za
 #SBATCH --mail-type=ALL
 
-echo "cores, pop_size, num_particles, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results/serial/ser_particles.txt
+echo "cores, pop_size, num_particles, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results2.0/serial/ser_particles.txt
 
 make particle
 
 # different number of particles
-for i in 50 100 200 500
+for i in 8 16 32 64 128
 do
-	/home/hpc36/CSC_HPC_Project/serial/particle 2000 100 100 $i 1 >> /home/hpc36/CSC_HPC_Project/Results/serial/ser_particles.txt
-	echo "omp: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results/serial/ser_log.txt
+	/home/hpc36/CSC_HPC_Project/serial/particle 2000 100 100 $i 1 >> /home/hpc36/CSC_HPC_Project/Results2.0/serial/ser_particles.txt
+	echo "omp: $i particles done" >> /home/hpc36/CSC_HPC_Project/Results2.0/serial/ser_log.txt
 done 
 
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/serial/ser_particles.txt
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/serial/ser_log.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/serial/ser_particles.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/serial/ser_log.txt
 
 rm /home/hpc36/CSC_HPC_Project/serial/particle
