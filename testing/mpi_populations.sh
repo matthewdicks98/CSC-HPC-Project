11677e9788a9cfb279649c526c46386942b99f3a
bmansvelt <64210209+bmansvelt@users.noreply.github.com>
2020-11-13 19:13:46 +0200

    Update mpi_populations.sh

--- testing/mpi_populations.sh
+++ testing/mpi_populations.sh
@@ -9,18 +9,18 @@
 
 module load mpi/openmpi-4.0.1
 
-echo "cores, pop_size, num_particles, migration_interval, migration_rate, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_population.txt
+echo "cores, pop_size, num_particles, migration_interval, migration_rate, final_fitness, gen_count, elapsed, elapsed/gen_count" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_population.txt
 
 make particle_mpi
 
 # different population size
-for i in 2000 5000 7000 10000 15000 30000 70000 100000
+for i in 2000 4000 8000 16000 32000 64000
 do 
-	mpirun /home/hpc36/CSC_HPC_Project/mpi/particle_mpi $i 100 100 50 1 50 0.05 >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_population.txt
+	mpirun /home/hpc36/CSC_HPC_Project/mpi/particle_mpi $i 100 100 50 1 20 0.05 >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_population.txt
 	echo "mpi: $i population done" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_log.txt
 done 
 
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_population.txt
-echo "-----" >> /home/hpc36/CSC_HPC_Project/Results/mpi/mpi_log.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_population.txt
+echo "-----" >> /home/hpc36/CSC_HPC_Project/Results2.0/mpi/mpi_log.txt
 
 rm /home/hpc36/CSC_HPC_Project/mpi/particle_mpi
